039ac86f60552ac445a72cd2fad76bbcaabf83f9
Rafael <rafael@gmail.com>
2023-11-28 12:07:31 -0300

    up_wsl2

---
+++ hackerRank/a_personalized_echo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+read userInput
+
+echo "Welcome $userInput"
---
+++ hackerRank/looping_odd_numbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+count=1
+
+while [ "$count" -le 99 ]
+do
+    if [ "$((count % 2))" -eq 1 ]; then
+        echo "$count"
+    fi
+
+    ((count++))
+done
+
